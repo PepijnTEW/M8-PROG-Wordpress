26d1829c16de84e37e595e1246a3ee59dff0ade6
PepijnTEW <37923@ma-web.nl>
2025-06-08 12:59:45 +0200

    fixed error

--- themes/myowntheme/functions.php
+++ themes/myowntheme/functions.php
@@ -1,8 +1,12 @@
-<php
+<?php
 function m8prog_enqueue_styles() {
-	wp_enqueue_style(
-		'm8prog-style',
-		get_stylesheet_uri()
-	);
+    // Load parent theme stylesheet first
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+
+    // Load child theme stylesheet
+    wp_enqueue_style('m8prog-style',
+        get_stylesheet_uri(),
+        array('parent-style')  // Make sure it loads after parent
+    );
 }
-add_action( 'wp_enqueue_scripts', 'm8prog_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'm8prog_enqueue_styles');
