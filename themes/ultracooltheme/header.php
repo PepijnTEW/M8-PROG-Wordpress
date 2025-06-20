1f18330e86b13481d24aae2fd0d4cf956bfc166d
PepijnTEW <37923@ma-web.nl>
2025-06-20 14:40:51 +0200

    les 4

--- themes/ultracooltheme/header.php
+++ themes/ultracooltheme/header.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html <?php language_attributes() ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ) ?>" />
@@ -7,4 +7,11 @@
 
 <body <?php body_class() ?>>
 <?php wp_body_open() ?>
-<nav>Nav</nav>
+<header id="site-nav">
+	<div id="site-nav__title"><a class="nav-item" id="nav-title" href="<?php echo home_url() ?>">The Chinese Kitchen</a></div>
+	<nav id="site-nav__menu">
+		<a class="nav-item link" href="<?php echo home_url() ?>">Home</a>
+        <a class="nav-item link" href="<?php echo get_post_type_archive_link('recepten') ?>">Recipes</a>
+        <a class="nav-item link" href="<?php echo home_url('/about') ?>">About</a>
+	</nav>
+</header>
--- themes/ultracooltheme/index.php
+++ themes/ultracooltheme/index.php
@@ -1,3 +1,16 @@
 <?php
 get_header();
-get_footer();
+?>
+<main class="container my-5">
+    <h1 class="mt-5">Dit is mijn eerste titel</h1>
+    <div class="col-lg-8 px-0">
+        <p class="fs-5">
+            Intro
+        </p>
+        <p>
+            Content
+        </p>
+    </div>
+</main>
+
+<?get_footer();
